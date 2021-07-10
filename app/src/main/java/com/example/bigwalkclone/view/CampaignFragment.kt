e0d17408de0dc361453bb23e1378d7819534fa12
HyeokJaeChoi <gurwo1007@gmail.com>
2021-07-10 19:09:56 +0900

    캠페인 정보를 표시할 CampaignFragment & CampaignViewModel 추가

---
+++ app/src/main/java/com/example/bigwalkclone/view/CampaignFragment.kt
@@ -0,0 +1,20 @@
+package com.example.bigwalkclone.view
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.bigwalkclone.R
+
+class CampaignFragment : Fragment() {
+    companion object {
+        fun newInstance() = CampaignFragment()
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
---
+++ app/src/main/java/com/example/bigwalkclone/view/CampaignViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.bigwalkclone.view
+
+import androidx.lifecycle.ViewModel
+
+class CampaignViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
