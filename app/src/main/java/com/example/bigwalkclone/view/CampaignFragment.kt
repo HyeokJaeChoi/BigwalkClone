89f2e2165c66352aa64baf3a01c3a10509a9c0e7
HyeokJaeChoi <gurwo1007@gmail.com>
2021-07-10 19:58:16 +0900

    메인화면 레이아웃 구성

--- app/src/main/java/com/example/bigwalkclone/view/CampaignFragment.kt
+++ app/src/main/java/com/example/bigwalkclone/view/CampaignFragment.kt
@@ -6,15 +6,35 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import com.example.bigwalkclone.R
+import com.example.bigwalkclone.databinding.CampaignFragmentBinding
 
 class CampaignFragment : Fragment() {
     companion object {
         fun newInstance() = CampaignFragment()
     }
 
+    private val campaignViewModel by viewModels<CampaignViewModel>()
+    private var _binding: CampaignFragmentBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = CampaignFragmentBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 }
